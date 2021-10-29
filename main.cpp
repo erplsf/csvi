589a8a87656a12da820488fd2251a2f91185d9e5
Andriy Mykhaylyk <andriy.mykhaylyk@klar.mx>
2021-10-29 15:02:49 +0200

    follow the tutorial

--- main.cpp
+++ main.cpp
@@ -1,5 +1,42 @@
+#include "vulkan/vulkan_core.h"
 #include <SDL.h>
 #include <SDL_vulkan.h>
+#include <__nullptr>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <vulkan/vulkan.h>
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+const std::vector<const char *> validationLayers = {"VK_LAYER_KHRONOS_validation"};
+#endif
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType,
+                                                    const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData, void *pUserData) {
+  std::cerr << "dbg: " << pCallbackData->pMessage << std::endl;
+
+  return VK_FALSE;
+}
+
+static VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT *pCreateInfo,
+                                             const VkAllocationCallbacks *pAllocator, VkDebugUtilsMessengerEXT *pDebugMessenger) {
+  auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+  if (func != nullptr) {
+    return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+  } else {
+    return VK_ERROR_EXTENSION_NOT_PRESENT;
+  }
+}
+
+static void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks *pAllocator) {
+  auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+  if (func != nullptr) {
+    func(instance, debugMessenger, pAllocator);
+  }
+}
 
 class App {
 public:
@@ -11,47 +48,184 @@ public:
   }
 
 private:
-  void initWindow(){
+  SDL_Window *_window;
+  VkInstance _instance;
+  VkDebugUtilsMessengerEXT _debugMessenger;
+  VkPhysicalDevice physicalDevice;
 
+  void setupDebugMessenger() {
+    VkDebugUtilsMessengerCreateInfoEXT createInfo;
+    populateDebugMessengerCreateInfo(createInfo);
+
+    if (CreateDebugUtilsMessengerEXT(_instance, &createInfo, nullptr, &_debugMessenger) != VK_SUCCESS) {
+      throw std::runtime_error("failed to set up debug messenger!");
+    }
+  }
+
+  void initWindow() {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    _window = SDL_CreateWindow("csvi", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_VULKAN | SDL_WINDOW_SHOWN);
+
+    // for (auto &str : _instance_extensions) {
+    //   std::cout << str << "\n";
+    // }
   };
-  void initVulkan();
-  void mainLoop();
-  void cleanup();
+
+  void initVulkan() {
+    createInstance();
+    if constexpr (enableValidationLayers) {
+      setupDebugMessenger();
+    }
+    pickPhysicalDevice();
+  };
+
+  void pickPhysicalDevice(){};
+
+  bool checkExtensionLayerSupport(const std::vector<const char *> requiredExtensions) {
+    uint32_t extensionCount;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::vector<VkExtensionProperties> availableExtensions(extensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, availableExtensions.data());
+
+    for (const char *extensionName : requiredExtensions) {
+      bool extensionFound = false;
+
+      for (const auto &extensionProperties : availableExtensions) {
+        // std::cout << layerProperties.layerName << "\n";
+        if (strcmp(extensionName, extensionProperties.extensionName) == 0) {
+          extensionFound = true;
+          break;
+        }
+      }
+
+      if (!extensionFound) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  bool checkValidationLayerSupport() {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    for (const char *layerName : validationLayers) {
+      bool layerFound = false;
+
+      for (const auto &layerProperties : availableLayers) {
+        // std::cout << layerProperties.layerName << "\n";
+        if (strcmp(layerName, layerProperties.layerName) == 0) {
+          layerFound = true;
+          break;
+        }
+      }
+
+      if (!layerFound) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  std::vector<const char *> getRequiredExtensions() {
+    unsigned int extensionCount = 0;
+    std::vector<const char *> extensions;
+
+    if (!SDL_Vulkan_GetInstanceExtensions(_window, &extensionCount, nullptr)) {
+      throw std::runtime_error("can't get count of required extensions from SDL");
+    }
+
+    extensions.resize(extensionCount);
+    if (!SDL_Vulkan_GetInstanceExtensions(_window, &extensionCount, extensions.data())) {
+      throw std::runtime_error("can't get list of required extensions from SDL");
+    };
+
+    if constexpr (enableValidationLayers) {
+      extensions.emplace_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    }
+
+    return extensions;
+  }
+
+  void createInstance() {
+    auto requiredExtensions = getRequiredExtensions();
+    if (!checkExtensionLayerSupport(requiredExtensions)) {
+      throw std::runtime_error("instance extensions requested, but not available!");
+    }
+
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "csvi";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+    createInfo.enabledExtensionCount = requiredExtensions.size();
+    createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+
+    if constexpr (enableValidationLayers) {
+      if (checkValidationLayerSupport()) {
+        createInfo.enabledLayerCount = validationLayers.size();
+        createInfo.ppEnabledLayerNames = validationLayers.data();
+
+        VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
+        populateDebugMessengerCreateInfo(debugCreateInfo);
+        createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT *)&debugCreateInfo;
+      } else {
+        throw std::runtime_error("validation layers requested, but not available!");
+      }
+    } else {
+      createInfo.enabledLayerCount = 0;
+    }
+
+    if (vkCreateInstance(&createInfo, nullptr, &_instance) != VK_SUCCESS) {
+      throw std::runtime_error("failed to create instance!");
+    }
+  }
+
+  void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT &createInfo) {
+    createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    createInfo.messageSeverity =
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+    createInfo.messageType =
+        VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+    createInfo.pfnUserCallback = debugCallback;
+  }
+
+  void mainLoop() {
+    bool run = true;
+    while (run) {
+      SDL_Event event;
+      if (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+          run = false;
+        }
+      }
+    }
+  };
+
+  void cleanup() {
+    if constexpr (enableValidationLayers) {
+      DestroyDebugUtilsMessengerEXT(_instance, _debugMessenger, nullptr);
+    }
+    vkDestroyInstance(_instance, nullptr);
+    SDL_Quit();
+  };
 };
 
 int main(int argc, char *argv[]) {
-  SDL_Init(SDL_INIT_VIDEO);
-
-  SDL_Window *window =
-      SDL_CreateWindow("csvi", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                       640, 480, SDL_WINDOW_VULKAN | SDL_WINDOW_SHOWN);
-
-  SDL_Renderer *renderer =
-      SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-  SDL_RenderClear(renderer);
-  SDL_RenderPresent(renderer);
-
-  // SDL_Event e;
-  // bool quit = false;
-  // while (!quit) {
-  //   while (SDL_PollEvent(&e)) {
-  //     if (e.type == SDL_QUIT) {
-  //       quit = true;
-  //     }
-  //     if (e.type == SDL_KEYDOWN) {
-  //       quit = true;
-  //     }
-  //     if (e.type == SDL_MOUSEBUTTONDOWN) {
-  //       quit = true;
-  //     }
-  //   }
-  // }
-
-  SDL_Delay(5000);
-
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-
-  return 0;
+  App app;
+  app.run();
 }
